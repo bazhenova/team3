ad013783dc993bb291bf1fa2a9ac942e04608255
bazhenova <anyusha2595@gmail.com>
2016-03-27 16:19:45 +0500

    update 2 searcher

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -1,3 +1,12 @@
-$("button").click(function () {
-    // alert("ok");
+$("#search").on("input", function () {
+    // console.log(this.value);
+    $.ajax({
+        type: "GET",
+        url: "/search-word",
+        data: { word: this.value },
+        success: function(msg){
+            console.log("Данные:" + msg);
+        }
+    });
 });
+
